fdffb7d577d0e834d9d892204e9765d3f4595c7b
Em Reynolds <47484942+emreynolds9@users.noreply.github.com>
2021-05-10 12:07:09 -0400

    Updated API Key in config.

--- static/js/config.js
+++ static/js/config.js
@@ -1,4 +1,4 @@
 // API key
-const API_KEY = "pk.eyJ1IjoiZW1yZXlub2xkczkiLCJhIjoiY2p4aHFxYzJsMGZ3NTNubWFvbTVlMGgybyJ9.1W1KWmwMFBQRICnnecRKog";
+const API_KEY = "pk.eyJ1IjoiZW1yZXlub2xkczkiLCJhIjoiY2tvaXNzZXN1MHgxMzJ2b3k0NXcwY285aSJ9.8a6RQ1dYcbeny-q8md30EQ";
 
   
